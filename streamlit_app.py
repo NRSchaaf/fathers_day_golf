b4fbd5ba14e91ad3041e27128f0703305109ee3f
Nathan Schaaf <108774884+NRSchaaf@users.noreply.github.com>
2026-06-21 20:04:25 -0400

    Remove unnecessary authentication parameters
    Removed 'authenticator' and 'token' parameters from the connection function.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,8 @@ st.set_page_config(page_title="Fathers Day Golf Manager", layout="wide")
 def get_connection():
     return snowflake.connector.connect(
         user=st.secrets["snowflake"]["user"],
+        password=st.secrets["snowflake"]["password"],
         account=st.secrets["snowflake"]["account"],
-        authenticator="oauth",
-        token=st.secrets["snowflake"]["token"],
         warehouse=st.secrets["snowflake"]["warehouse"],
         database=st.secrets["snowflake"]["database"],
         schema=st.secrets["snowflake"]["schema"],
